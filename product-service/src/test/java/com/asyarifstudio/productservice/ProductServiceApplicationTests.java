c52f5177949584261b11fc387db04bf22e536fdf
asyarifstudio <asyarif.studio@gmail.com>
2023-04-01 17:04:12 +0800

    initialize the project

---
+++ product-service/src/test/java/com/asyarifstudio/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyarifstudio.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
